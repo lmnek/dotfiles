07720bee615bfaa513dad1f6525e9f606e927395
Krystof Gartner <krystofgartner@proton.me>
2025-05-13 23:38:58 +0200

    minor thingies

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -2,6 +2,16 @@
 -- - extend the functionality of Yazi (e.g. status bar behavior)
 -- - customize plugins
 
+-- Show symlink in statusbar (from 'tips' in docs)
+Status:children_add(function(self)
+    local h = self._current.hovered
+    if h and h.link_to then
+        return " -> " .. tostring(h.link_to)
+    else
+        return ""
+    end
+end, 3300, Status.LEFT)
+
 -- cmd for installing/listing/updating plugins: "ya pack ..."
 -- nice plugins list: https://yazi-rs.github.io/docs/resources
 
