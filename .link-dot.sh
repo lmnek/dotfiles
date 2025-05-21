65dd7d5567c490864945ba1770865b34c27b0394
Krystof Gartner <krystofgartner@proton.me>
2025-05-21 22:15:17 +0200

    obsidian: migrated configs 🌟 (finally)

--- .link-dot.sh
+++ .link-dot.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-# stow -d ~/repos/dotfiles/  -t ~/ ~/repos/dotfiles/
-cd ~/repos/dotfiles/ && stow -t ~/ .
+set -e # Exit immediately if a command exits with a non-zero status
+
+REPOS_PATH="$HOME/repos"
+DOTFILES_PATH="$REPOS_PATH/dotfiles"
+OBSIDIAN_VAULT_PATH="$REPOS_PATH/obsidian/Lemonek/"
+
+cd $DOTFILES_PATH
+
+stow -t "$HOME" .
+
+# link obsidian settings (to keep this repos structure clean)
+stow -t "$OBSIDIAN_VAULT_PATH/.obsidian" .obsidian
