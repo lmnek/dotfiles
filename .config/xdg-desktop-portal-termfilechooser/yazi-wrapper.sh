0cf0c6bf4d8c89722b7989a2124702b4ea304aac
Krystof Gartner <krystofgartner@proton.me>
2025-05-27 23:20:22 +0200

    termfilechooser: adapting to ghostty + update
    + works with firefox now

--- .config/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
+++ .config/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
@@ -1,56 +1,113 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+# Source: https://github.com/boydaihungst/xdg-desktop-portal-termfilechooser/blob/main/contrib/yazi-wrapper.sh
+# -> adapted to ghostty terminal
+
+set -x
 
 # This wrapper script is invoked by xdg-desktop-portal-termfilechooser.
+#
+# Inputs:
+# 1. "1" if multiple files can be chosen, "0" otherwise.
+# 2. "1" if a directory should be chosen, "0" otherwise.
+# 3. "0" if opening files was requested, "1" if writing to a file was
+#    requested. For example, when uploading files in Firefox, this will be "0".
+#    When saving a web page in Firefox, this will be "1".
+# 4. If writing to a file, this is recommended path provided by the caller. For
+#    example, when saving a web page in Firefox, this will be the recommended
+#    path Firefox provided, such as "~/Downloads/webpage_title.html".
+#    Note that if the path already exists, we keep appending "_" to it until we
+#    get a path that does not exist.
+# 5. The output path, to which results should be written.
+#
+# Output:
+# The script should print the selected paths to the output path (argument #5),
+# one path per line.
+# If nothing is printed, then the operation is assumed to have been canceled.
 
-# See `common.sh` for the description of the input arguments and the output format.
-
-set -eu
-
-. "${0%/*}/common.sh"  # source common functions
-
-multiple=$1
-directory=$2
-save=$3
-path=$4
-out=$5
-
-cmd='yazi'
-termcmd=$(default_termcmd)
+multiple="$1"
+directory="$2"
+save="$3"
+path="$4"
+out="$5"
+cmd="/usr/bin/yazi"
 
 if [ "$save" = "1" ]; then
-	create_save_file "$path" "$out"
-	set -- --chooser-file="$out" "$path"
+  TITLE="Save File:"
 elif [ "$directory" = "1" ]; then
-	set -- --chooser-file="$out" --cwd-file="$out"
-	# Returns either the dir explicitly selected (with e.g. <Enter>), or the last dir yazi was in before exiting.
+  TITLE="Select Directory:"
 else
-	set -- --chooser-file="$out"
+  TITLE="Select File:"
 fi
 
-$termcmd $cmd "$@"
+quote_string() {
+  local input="$1"
+  echo "'${input//\'/\'\\\'\'}'"
+}
 
-# #!/bin/bash
-# set -x
-# multiple="$1"
-# directory="$2"
-# save="$3"
-# path="$4"
-# out="$5"
-# cmd="yazi"
-# termcmd="${TERMCMD:-wezterm start --always-new-process}"
-#
-# if [ "$save" = "1" ]; then
-#     ARGS=--chooser-file="$out"
-# elif [ "$directory" = "1" ]; then
-#     ARGS=--cwd-file="$out"
-# elif [ "$multiple" = "1" ]; then
-#     ARGS=--chooser-file="$out"
-# else
-#     ARGS=--chooser-file="$out"
-# fi
-#
-# $termcmd $cmd $ARGS
-#
-# if [ "$save" = "1" ] && [ ! -s "$out" ]; then
-#     /usr/bin/rm "$path"
-# fi
+# termcmd="${TERMCMD:-/usr/bin/ghostty --title=$(quote_string "$TITLE")}" 
+# dont use default.. -> can add title:
+termcmd="/usr/bin/ghostty --class='com.lmnk.termfilechooser' --title=$(quote_string "$TITLE")"
+
+cleanup() {
+  if [ -f "$tmpfile" ]; then
+    /usr/bin/rm "$tmpfile" || :
+  fi
+  if [ "$save" = "1" ] && [ ! -s "$out" ]; then
+    /usr/bin/rm "$path" || :
+  fi
+}
+
+trap cleanup EXIT HUP INT QUIT ABRT TERM
+
+if [ "$save" = "1" ]; then
+  tmpfile=$(/usr/bin/mktemp)
+
+  # Save/download file
+  /usr/bin/printf '%s' 'xdg-desktop-portal-termfilechooser saving files tutorial
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!                 === WARNING! ===                 !!!
+!!! The contents of *whatever* file you open last in !!!
+!!! yazi will be *overwritten*!                    !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+Instructions:
+1) Move this file wherever you want.
+2) Rename the file if needed.
+3) Confirm your selection by opening the file, for
+   example by pressing <Enter>.
+
+Notes:
+1) This file is provided for your convenience. You can
+	 only choose this placeholder file otherwise the save operation aborted.
+2) If you quit yazi without opening a file, this file
+   will be removed and the save operation aborted.
+' >"$path"
+  set -- --chooser-file="$(quote_string "$tmpfile")" "$(quote_string "$path")"
+elif [ "$directory" = "1" ]; then
+  # upload files from a directory
+  # Use this if you want to select folder by enter a.k.a yazi keybind for 'open' funtion ('run = "open") .
+  set -- --chooser-file="$out" "$path"
+  # Use this if you want to select folder by 'quit' function in yazi.
+  # set -- --cwd-file="$(quote_string "$out")" "$(quote_string "$path")"
+elif [ "$multiple" = "1" ]; then
+  # upload multiple files
+  set -- --chooser-file="$(quote_string "$out")" "$(quote_string "$path")"
+else
+  # upload only 1 file
+  set -- --chooser-file="$(quote_string "$out")" "$(quote_string "$path")"
+fi
+
+echo "$termcmd -e '$cmd $@'"
+eval "$termcmd -e '$cmd $@'"
+
+# case save file
+if [ "$save" = "1" ] && [ -s "$tmpfile" ]; then
+  selected_file=$(/usr/bin/head -n 1 "$tmpfile")
+  # Check if selected file is placeholder file
+  if [ -f "$selected_file" ] && /usr/bin/grep -qi "^xdg-desktop-portal-termfilechooser saving files tutorial" "$selected_file"; then
+    /usr/bin/echo "$selected_file" >"$out"
+    path="$selected_file"
+  fi
+fi
