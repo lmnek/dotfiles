b071badccb66a55247a378325327f291f305f569
Krystof Gartner <t-kgartner@microsoft.com>
2025-08-14 10:28:33 +0200

    nvim: fix ConformInfo error

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -21,7 +21,7 @@ return {
                 python = { "isort", "black" },
                 rust = { "rustfmt" },
                 -- You can use a sub-list to tell conform to run *until* a formatter is found.
-                javascript = { { "prettierd", "prettier" } },
+                -- javascript = { { "prettierd", "prettier" } },
             },
         })
 
