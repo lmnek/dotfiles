19001e9ebfbe632f66319b4d6c26ec218ccfb329
Krystof Gartner <krystofgartner@proton.me>
2025-09-15 18:26:43 +0200

    nvim: trouble visual mods

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,33 +1,43 @@
-local minimalTreeModeOpts = {
-    title = false,
-    groups = {},
-}
-
 -- Super pretty diagnostic/quickfix/telescope list
--- TODO:
---  - position nicely with edgy.nvim (+ neotree, undolist)
---  - TODO list
+-- TODO: position nicely with edgy.nvim (+ neotree, undolist)
 return {
     "folke/trouble.nvim",
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
         focus = true, -- focus on toggle
         warn_no_results = true,
-        auto_close = true,
-        indent_guides = false,
+        auto_close = false,
+        indent_guides = true,
         preview = { border = "" },
         title = false,
         modes = {
-            symbols = minimalTreeModeOpts,
-            lsp = minimalTreeModeOpts,
-            todo = {
+            diagnostics = {
+                auto_close = true,
+            },
+            symbols = {
+                title = false,
                 groups = {},
-                format = "{todo_icon} {text} {filename} {pos}",
+            },
+            todo = {
+                auto_close = true,
+                groups = {},
+                format = "{todo_icon} {text} {filename}",
                 sort = { "tag", "filename", "pos", "message" },
                 filter = {
                     ['not'] = { tag = "NOTE" },
                 },
-            }
+            },
+            lsp = {
+                title = false,
+                groups = {},
+            },
+            -- concrete items for LSP mode -> remove item.client and pos
+            lsp_base = { format = "{text:ts}" },
+            lsp_incoming_calls = { format = "{text:ts}" },
+            lsp_outgoing_calls = { format = "{text:ts}" },
+        },
+        icons = {
+            indent = { ws = " ", }, -- saving width space
         },
     },
     -- cmd: Trouble [mode] [action] [options]
