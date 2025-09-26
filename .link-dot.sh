b9d5b1ec555ed3307f049ffc347bd0afed85c8b4
Krystof Gartner <krystofgartner@proton.me>
2025-09-26 15:06:35 +0200

    Introduce tilde directory

--- .link-dot.sh
+++ .link-dot.sh
@@ -6,9 +6,9 @@ REPOS_PATH="$HOME/repos"
 DOTFILES_PATH="$REPOS_PATH/dotfiles"
 OBSIDIAN_VAULT_PATH="$REPOS_PATH/obsidian/Lemonek/"
 
-cd $DOTFILES_PATH
-
+cd "$DOTFILES_PATH/tilde"
 stow -t "$HOME" .
 
 # link obsidian settings (to keep this repos structure clean)
+cd "$DOTFILES_PATH"
 stow -t "$OBSIDIAN_VAULT_PATH/.obsidian" .obsidian
