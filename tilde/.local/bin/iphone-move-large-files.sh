882718aea9abcd5627a7e2585c3c51724b4fa8b0
Kryštof Gärtner <krystofgartner@proton.me>
2025-11-20 14:57:09 +0100

    moved bin into .local

--- tilde/.local/bin/iphone-move-large-files.sh
+++ tilde/.local/bin/iphone-move-large-files.sh
@@ -3,6 +3,6 @@
 # my version
 du -a ~/iPhone/DCIM | sort -n -r | head -n 30 | grep .MOV | awk '{ print $2 }' | parallel mv {} ~/vids/iphone/
 
-# chat-GPT version
+# generated version
 # find ~/iPhone -type f \( -iname '*.mov' -o -iname '*.mp4' \) -printf '%s %p\n' | sort -n -r | head -n 30 | awk '{print $2}' | xargs -I{} mv {} ~/vids/iphone/
 
