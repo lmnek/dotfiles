e6e1c73a30ef14c23d8c769bf9f7e80c46fdef34
Kryštof Gärtner <krystofgartner@proton.me>
2025-12-28 19:19:02 +0100

    vipe: v2.0

--- tilde/.local/bin/vipe.sh
+++ tilde/.local/bin/vipe.sh
@@ -1,44 +1,48 @@
 #!/bin/bash
 set -eu
 
+# Example usage:
+# echo "input from pipe" | vipe.sh --launch -- -c 'norm o aloha' | bat
+
 # Defaults
-MODE="inline"
-TERMAPP="xterm" # or "ghostty"
-TERM_CLASS=""
+TERMAPP="xterm" # or ghostty
+TERM_CLASS="vipe" # for i3wm window matching
 NVIM_OPTS=(
     -c startinsert
     -c 'lua vim.keymap.set("i","<S-CR>","<Esc>:wq!<CR>",{silent=true})'
-)
+) 
 
-# Argument Parsing
+# Modes: inline (current terminal) or term (launch external)
+MODE="inline"
+
+# Arg Parsing
 while [[ $# -gt 0 ]]; do
     case "$1" in
         -l|--launch) MODE="term" ;;
         -p|--picker) MODE="term"; TERM_CLASS="vipe_picker"; NVIM_OPTS+=("-u" "NORC") ;;
         --class)     TERM_CLASS="$2"; shift ;;
-        --nvim-opts) NVIM_OPTS+=("$2"); shift ;;
-        *)           break ;;
+        --) shift; NVIM_OPTS+=("$@"); break ;; # add everything remaining to nvim options
+        *) echo "Unknown internal argument: $1." >&2; exit 1 ;;
     esac
     shift
 done
 
+# Create temp file & handle input
+tmp="$(mktemp)"; trap 'rm -f "$tmp"' EXIT
+[ -t 0 ] || cat >"$tmp" # If stdin is NOT a tty (it's a pipe) => fill tmp file with input pipe
 
-# Create tmp file and fill with input pipe (if present)
-tmp="$(mktemp)"
-trap 'rm -rf "$tmp"' EXIT INT HUP TERM
-fill() { [ -t 0 ] && : >"$1" || cat >"$1"; }
-fill "$tmp"
+# Execution construction
+if [[ "$MODE" == "term" ]]; then
+    TERM_FLAGS=(); [[ -n "$TERM_CLASS" ]] && TERM_FLAGS+=(-name "$TERM_CLASS") # or --class for ghostty 
 
-# Edit with nvim (same terminal or launch new terminal)
-if [ "$MODE" = "inline" ]; then
-    nvim "${NVIM_OPTS[@]}" "$tmp" </dev/tty >/dev/tty
-elif [ "$MODE" = "term" ]; then
-    TERM_FLAGS=()
-    [[ -n "$TERM_CLASS" ]] && TERM_FLAGS+=(-name "$TERM_CLASS") # or --class for ghostty
-
-    "$TERMAPP" "${TERM_FLAGS[@]}" -e bash -lc 'nvim "$@"' -- \
-        "${NVIM_OPTS[@]}" "$tmp" </dev/null >/dev/null 2>&1
+    CMD=("$TERMAPP" "${TERM_FLAGS[@]}" -e nvim)
+elif [[ "$MODE" == "inline" ]] then
+    CMD=(nvim)
+    exec < /dev/tty > /dev/tty # requires TTY redirection so nvim takes over terminal 
 fi
 
-# For output pipe 
+# Nvim input / editing
+"${CMD[@]}" "${NVIM_OPTS[@]}" "$tmp"
+
+# For output pipe  
 cat "$tmp"
